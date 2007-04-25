a607af336789f9096311f28d8703aa7f55bf5129
ted.husted <ted.husted@0be7c2d7-182d-0410-87d6-19f808efe882>
2007-04-25 17:52:59 +0000

    Issue 13 - Add fields to data for autocomplete.

--- trunk/examples/phonebook/Home.js
+++ trunk/examples/phonebook/Home.js
@@ -4,12 +4,14 @@ YAHOO.my.oLogReader = new YAHOO.widget.LogReader("elLogReader");
 
 // Static data for low-level testing    
 YAHOO.my.oLocalData = { 
-    "result" : [['c5b6bbb1-66d6-49cb-9db6-743af6627828', 'Clinton', 'William', '555-743-7828', 'bubba', '08/19/1992', 37.5, '0'],
-    ['7c424227-8e19-4fb5-b089-423cfca723e1', 'Roosevelt', 'Theodore', '555-743-8942', 'bull', '09/14/2002', 37.5, '0'],
-    ['9320ea40-0c01-43e8-9cec-8fb9b3928c2c', 'Kennedy', 'John F.', '555-743-3928', 'fitz', '05/29/1987', 37.5, '0'],
-    ['3b27c933-c1dc-4d85-9744-c7d9debae196', 'Pierce', 'Franklin', '555-743-7919', 'benji', '11/18/1984', 35, '0'],
-    ['554ff9e7-a6f5-478a-b76b-a666f5c54e40', 'Jefferson', 'Thomas', '555-743-5440', 'monty', '07/04/1976', 37.5, '0']]
-};
+    result : [
+		{entry_key: 'c5b6bbb1-66d6-49cb-9db6-743af6627828', last_name: 'Clinton', first_name: 'William', extension: '555-743-7828', username: 'bubba', hired: '08/19/1992', hours: 37.5, editor: '0'},
+	    {entry_key: '7c424227-8e19-4fb5-b089-423cfca723e1', last_name: 'Roosevelt', first_name: 'Theodore', extension: '555-743-8942', username: 'bull', hired: '09/14/2002', hours: 37.5, editor: '0'},
+		{entry_key: '9320ea40-0c01-43e8-9cec-8fb9b3928c2c', last_name: 'Kennedy', first_name: 'John F.', extension: '555-743-3928', username: 'fitz', hired: '05/29/1987', hours: 37.5, editor: '0'},
+    	{entry_key: '3b27c933-c1dc-4d85-9744-c7d9debae196', last_name:'Pierce', first_name: 'Franklin', extension: '555-743-7919', username: 'benji', hired: '11/18/1984', hours: 35, editor:'0'},
+    	{entry_key: '554ff9e7-a6f5-478a-b76b-a666f5c54e40', last_name:'Jefferson', first_name: 'Thomas', extension: '555-743-5440', username: 'monty', hired: '07/04/1976', hours: 37.5, editor:'0'}
+		]
+	};
 
 YAHOO.my.Events = function() {
     this.createEvent("entryList")
@@ -23,9 +25,9 @@ YAHOO.my.events.onEntryListReturn = function(oData) {
     YAHOO.my.events.fireEvent("entryList", oData);	
 }
 
-YAHOO.my.PhoneBook = function() {
-    var phonebook = object(FlevBase);                   
-    phonebook.oColumnHeaders = [
+YAHOO.my.Phonebook = function() {
+    var oPhonebook = object(FlevBase);                   
+    oPhonebook.oColumnHeaders = [
         {key:"first_name", text:"First Name", sortable:true, resizeable:true, editor:"textbox", formClassName: "required", formTitle: "Enter employee's first name"},  
         {key:"last_name", text:"Last Name", sortable:true, resizeable:true, editor:"textbox", formClassName: "required", formTitle: "Enter employee's last name"},  
         {key:"extension", text:"Extension", sortable:true, resizeable:true, editor:"textbox", formClassName: "required", formTitle: "Enter extension or telephone number"},  
@@ -34,7 +36,7 @@ YAHOO.my.PhoneBook = function() {
         {key:"hours", text:"Hours", sortable:true, resizeable:true, editor:"textbox", formClassName: "required, validate-number", formTitle: "Enter hours scheduled per work week as a number"},  
     ];       
 
-    phonebook.oConfigs = {
+    oPhonebook.oConfigs = {
         caption: "EntryList", 
         summary: "List of matching entries",
         paginator:true,
@@ -45,22 +47,22 @@ YAHOO.my.PhoneBook = function() {
         rowSingleSelect: true                
     };            
 
-    phonebook.oResponseSchema = {
-        fields: ["entry_key","first_name","last_name","extension","username","hired","hours","editor"]
+    oPhonebook.oResponseSchema = {
+        fields: ["entry_key","last_name","first_name","extension","username","hired","hours","editor"]
     };            
 
-    phonebook.sName = "last_name";
+    oPhonebook.sName = "last_name";
     
-    phonebook.sDataTable = "elDataTable";
-    phonebook.sDataForm = "elDataForm";
-    phonebook.sTabView = "elTabView";
-    phonebook.sForm = "elForm";
+    oPhonebook.sDataTable = "elDataTable";
+    oPhonebook.sDataForm = "elDataForm";
+    oPhonebook.sTabView = "elTabView";
+    oPhonebook.sForm = "elForm";
 
-    YAHOO.my.events.subscribe("entryList", phonebook.load, phonebook);    
+    YAHOO.my.events.subscribe("entryList", oPhonebook.load, oPhonebook);    
     // Home.rpc.entryList(YAHOO.my.events.onEntryListReturn).call(ANVIL.channel); // live database
     YAHOO.my.events.onEntryListReturn(YAHOO.my.oLocalData); // static data
 
-    return phonebook;
+    return oPhonebook;
 };
-YAHOO.augment(YAHOO.my.PhoneBook, YAHOO.util.EventProvider);
-YAHOO.my.phonebook = new YAHOO.my.PhoneBook();
+YAHOO.augment(YAHOO.my.Phonebook, YAHOO.util.EventProvider);
+YAHOO.my.oPhonebook = new YAHOO.my.Phonebook();
