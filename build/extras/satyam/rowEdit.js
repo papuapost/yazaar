b30776278aa932090dfa13a2f6f4862d945274ef
ted.husted <ted.husted@0be7c2d7-182d-0410-87d6-19f808efe882>
2007-05-14 18:16:16 +0000

    Issue 21 - Add style sheet. The "concept test" example is running under FF, but IE won't open it. There is some browser sniffing going on to create the form, which might be the culprit.

--- build/extras/satyam/rowEdit.js
+++ build/extras/satyam/rowEdit.js
@@ -1,272 +1,231 @@
 
 /**
-Ok, it's done and posted at:
-
-https://sourceforge.net/tracker/index.php?func=detail&aid=1717216&group_id=165715&atid=836479
-
-
-So, this is how you would use it:
-
-Assuming dt is a global reference to an existing DataTable instance,
-first, once in the document, you create the form within a container, in this
-case, with an id of 'editForm'.
-
- dt.createEditForm('editForm');
-
-Then once for each record to be edited, you make the container visible and
-call showEditFormgiving it the record to be edited (here, the first record).
-Here, I am giving it an extra argument, an object with a property
-idExtraInfo which will be converted to a hidden input element with name
-idExtraInfo and value 123 and inserted into the form
-
- dt.showEditForm(dt.getRecordSet().getRecord(0),{idExtraInfo:123});
-
-You may subscribe to any or all of the following events. They are all
-triggered in succession by the submit event of the form.  If any of the
-validate event fails (returns false) the rest will not be triggered.
-
-This will be fired for each cell.  It will receive the input element (input,
-textarea, select box), the original value, a reference to the column (for
-whatever information you might need from it) and to the original record.  It
-will trigger only for fields in the form, not for all of the ones on the
-record, nor for the hidden fields.
-
- dt.subscribe('cellValidateEvent', function(args) {
-  console.log('cellValidateEvent', args);
-  return true;
- });
-
-
-This one fires once for the whole form.  It receives a reference to the
-form, the old data as an object of name:value  sets, the new data and the
-original record.
-
- dt.subscribe('formValidateEvent', function(args) {
-  console.log('formValidateEvent', args);
-  return true;
- });
-
-Finally, if everything validates, the formSubmitEvent fires.  If not
-subscribed, the form will be submitted as forms usually do.  If subscribed
-and returns true, it will also submit, if false, it cancels the submit (as
-per normal DOM standards).   It receives the form, the old data, the new
-data and the record, which is now updated with the new values (which should
-also be reflected in the browser).
-
-As a convenience, the postEditForm method is provided to make it simple to
-submit the form via XHR.  You just set it as the callback function for the
-event and add an extra argument with the callback object, as described for
-the asyncRequest method.
-
- dt.subscribe('formSubmitEvent', dt.dataTable.postEditForm, {
-  success:function() {
-   console.log('success',this)
-  },
-  failure:function() {
-   console.log('failure',this)
-  }
- });
-
-
-It still has all the references to DataTypes, which is what I use, but if
-you don't use 'edit:true' in the column descriptions, it should not
-complain, otherwise, let me know.
-
-Satyam  */
+ * A reference to the form created by createEditForm.
+ *
+ * @property _rowEditForm
+ * @type HTMLElement (form)
+ * @private
+ */
+YAHOO.widget.DataTable.prototype._rowEditForm = undefined;
 
 /**
-
-https://sourceforge.net/tracker/index.php?func=detail&aid=1717216&group_id=165715&atid=836479
-
-I wanted to have the ability to edit a full row of cells of a
-DataTable and came up with the following methods:
-
-The first, createEditForm takes as its arguments at least the name or
-element which will contain the form.
-
-The url argument, if exists, will be assigned to the form.action attribute.
-If url is absent and the DataTable was filled via a JSON request, the url
-will be taken from the DataSource.
-
-The onSubmitCallback argument is a reference to a function that will be
-called on the form onSubmit event. If missing, the form will be processed
-as usual. A function to do a submit via Connection Manager is provided
-(see later: postEditForm).
-
-Finally the arguments argument has any arguments that the callback function
-might require. When using the provided postEditForm method, they should be
-the regular arguments to an asyncRequest with its success, failure, scope,
-etc properties.
-
-The function creates a form and fills it with label and input elements for
-each column that has a 'editor:textbox','editor:textarea' or 'edit:true'
-attribute. It uses the 'text' attribute for labels and the 'key' attribute
-for field names.
-
-The form is assigned a className 'yui-dt-edit-form' to allow formatting of
-its contents via CSS.
-
-Another method is provided to load the form with data (see below)
-
- * @param {Object} elContainer
- * @param {Object} url
- * @param {Object} onSubmitCallback
- * @param {Object} arguments
+ * A reference to the record being edited
+ *
+ * @property _rowEditRecord
+ * @type YAHOO.widget.Record
+ * @private
  */
+YAHOO.widget.DataTable.prototype._rowEditRecord = undefined;
 
-YAHOO.widget.DataTable.prototype.createEditForm = function (elContainer,url,onSubmitCallback,args) {
+/**
+ * Holds a copy of the original values before edit
+ *
+ * @property _oldEditRowValues
+ * @type object
+ * @private
+ */
+YAHOO.widget.DataTable.prototype._oldEditRowValues = {};
+
+
+/**
+ * Creates a form within the container given with all fields marked for edit.  
+ * If an url is not provided it will try to take the one from the DataSource, if available
+ * The submit button for the form will be labeled Ok unless the third argument is provided.
+ *
+ * @method createEditForm
+ * @param elContainer {String || HTMLElement}  ID or HTML element which will contain the form
+ * @param url {String} URL to submit the form to.
+ * @param submitButtonLabel {String} Record ID or record index
+ */
+ 
+YAHOO.widget.DataTable.prototype.createEditForm = function(elContainer,url,submitButtonLabel) {
 	elContainer = YAHOO.util.Dom.get(elContainer);
 	var elForm = document.createElement('form');
 	YAHOO.util.Dom.addClass(elForm,'yui-dt-edit-form');
-	this.recordEditForm = elForm;
+	this._rowEditForm = elForm;
 	elContainer.appendChild(elForm);
 	elForm.setAttribute('method','post');
 	elForm.setAttribute('action',url || (this.dataSource.dataType == YAHOO.util.DataSource.TYPE_XHR)?this.dataSource.liveData:'');
 	for (var i = 0;i<this._oColumnSet.keys.length;i++) {
 		var oColumn = this._oColumnSet.keys[i];
 		switch (oColumn.editor) {
-			case 'textbox':
-				var elLabel = document.createElement('label');
-				elLabel.setAttribute('for',oColumn.key);
-				elLabel.innerHTML = oColumn.text || oColumn.key;
-				elForm.appendChild(elLabel);
-	
-				var elTextbox = document.createElement("input");
+		case 'textbox':
+			var elLabel = document.createElement('label');
+			elLabel.setAttribute('for',oColumn.key);
+			elLabel.innerHTML = oColumn.text || oColumn.key;
+			elForm.appendChild(elLabel);
+			
+			if (navigator.appName == 'Microsoft Internet Explorer') {
+				elForm.appendChild(document.createElement('<input name ="'  + oColumn.key + '">'));
+			} else {
+			    var elTextbox = document.createElement("input");
 				elTextbox.setAttribute('name',oColumn.key);
 				elForm.appendChild(elTextbox);
-				break;
-			case 'textarea':
-				var elLabel = document.createElement('label');
-				elLabel.setAttribute('for',oColumn.key);
-				elLabel.innerHTML = oColumn.text || oColumn.key;
-				elForm.appendChild(elLabel);
-	
-				var elTextArea = document.createElement("textarea");
+			}
+			
+			break;
+		case 'textarea':
+			var elLabel = document.createElement('label');
+			elLabel.setAttribute('for',oColumn.key);
+			elLabel.innerHTML = oColumn.text || oColumn.key;
+			elForm.appendChild(elLabel);
+			
+			if (navigator.appName == 'Microsoft Internet Explorer') {
+				elForm.appendChild(document.createElement('<TEXTAREA NAME="' + oColumn.key + '">'));
+			} else {
+				var elTextArea = document.createElement('textarea');
 				elTextArea.setAttribute('name',oColumn.key);
 				elForm.appendChild(elTextArea);
-				break;
-			default:
-				if (oColumn.edit) {
-					YAHOO.widget.DataTypes.createFormField(elForm,oColumn);
-				}
-				break;
+			}
+			break;
+		default:
+			if (oColumn.edit) {
+				oColumn._dataType.createFormField(elForm,oColumn);
+			}
+			break;
 		}
 	}
 	var buttonSubmit = document.createElement('input');
-	buttonSubmit.type='submit';
-	buttonSubmit.value= 'OK'; // args.submitButtonLabel || 'Ok'; // FIXME: "args has no properties"
+	buttonSubmit.setAttribute('type','submit');
+	buttonSubmit.setAttribute('value', submitButtonLabel || 'Ok');
 	elForm.appendChild(buttonSubmit);
-	if (onSubmitCallback) {	
-		YAHOO.util.Event.on(elForm,'submit',onSubmitCallback,args,this,true);
-	}
+	
+	YAHOO.util.Event.on(elForm,'submit',this._submitForm,this,true);
+	this.createEvent('formValidateEvent');
+	this.createEvent('formSubmitEvent');
 };
 
-
 /**
-
-Notice that the onSubmitCallback function is executed in the scope of the
-DataTable instance.
-
-The following function is called to fill the form with data.
-
-The function takes a reference to a particular record which will be loaded
-into the value of the input elements.
-
-It takes a second argument, which is expected to be an object of name:value
-pairs, which will be converted to <input type=hidden> elements. In each
-call, prior hidden elements are removed.
-
- * @param {Object} oRecord
- * @param {Object} extra
+ * Fills the previous form with values from the given recordCreates a form within the container given with all fields marked for edit.  
+ * If the second argument is given, it will create hidden input elements taking the property names and values from the object
+ *
+ * @method showEditForm
+ * @param oRecord {YAHOO.widget.Record}  DataTable record to be edited
+ * @param extra {Object} Object containing name:value pairs to be inserted into the form as hidden input elements
  */
+ 
 YAHOO.widget.DataTable.prototype.showEditForm = function(oRecord,extra) {
-	var elForm = this.recordEditForm,i,elHidden;
+	var elForm = this._rowEditForm,i,elHidden;
+	this._rowEditRecord = oRecord;
 	for (var i = 0; i < elForm.childNodes.length;i++) {
 		var el = elForm.childNodes[i];
 		if (el.nodeType === 1 && el.tagName.toUpperCase() === 'INPUT' && el.type === 'hidden') {
 			elForm.removeChild(el);
 		}
 	}
+			
+		
 	if (extra) {
 		for (i in extra) {
-			elHidden = document.createElement('input');
-			elHidden.setAttribute('type','hidden');
-			elHidden.setAttribute('name',i);
+			if (navigator.appName == 'Microsoft Internet Explorer') {
+				elHidden = document.createElement('<input type="hidden" name ="'  + i + '">');
+			} else {
+				elHidden = document.createElement('input');
+				elHidden.setAttribute('type','hidden');
+				elHidden.setAttribute('name',i);
+			}
 			elHidden.setAttribute('value',extra[i]);
 			elForm.appendChild(elHidden);
 		}
 	}
+			
 	
 	for (i = 0;i<this._oColumnSet.keys.length;i++) {
 		var oColumn = this._oColumnSet.keys[i];
+		this._oldEditRowValues[oColumn.key] = oRecord[oColumn.key];
 		switch (oColumn.editor) {
-			case 'textbox':
-				elForm[oColumn.key].setAttribute('value',oRecord[oColumn.key]);
-				break;
-			case 'textarea':
-				elForm[oColumn.key].innerHTML = oRecord[oColumn.key];
-				break;
-			default:
-				if (oColumn.edit) {
-					YAHOO.widget.DataTypes.showFormField(elForm,oRecord,oColumn);
-				}
-				break;
+		case 'textbox':
+			elForm[oColumn.key].setAttribute('value',oRecord[oColumn.key]);
+			break;
+		case 'textarea':
+			elForm[oColumn.key].innerHTML = oRecord[oColumn.key];
+			break;
+		default:
+			if (oColumn.edit) {
+				oColumn._dataType.showFormField(elForm[oColumn.key],oRecord,oColumn);
+			}
+			break;
 		}
 	}
 };
 
-
 /**
-
-The postEditForm method submits the form via Connection Manager. 'this'
-refers to the DataTable instance.
-
- * @param {Object} ev
- * @param {Object} arguments
+ * Function called when the user submits the form
+ * It will read the values from the form, fire cellValidateEvent for each field,
+ * fire formValidateEvent for the whole form and if no error is found so far (for all validate, true means it does validate Ok)
+ * it will update the DataTable and fire the event formSubmitEvent.  
+ * If that event returns true, it means it is ok to allow the regular form submit
+ * 
+ * @method _submitForm
+ * @param ev {Event object}
  */
-YAHOO.widget.DataTable.prototype.postEditForm = function(ev,arguments) {
+
+
+YAHOO.widget.DataTable.prototype._submitForm = function (ev) {
+	var elForm = this._rowEditForm;
+	var cols = this._oColumnSet.keys;
+	var oRecord = this._rowEditRecord;
+	var newValues = {};
+	var go = true;
+	for (var i = 0;i < cols.length;i++) {
+		var col = cols[i],field = elForm[col.key];
+		if ((col.edit || col.editor) && field) {
+			newValues[col.key] = col._dataType.getFormValue(field,col);
+			if (col._dataType.validateForm(field,col)
+				&& this.fireEvent("cellValidateEvent",{
+					target:field,
+					oldData:this._oldEditRowValues[col.key],
+					oColumn:col,oRecord:oRecord
+			})) {
+				col._dataType.showFieldError(field,false);
+			} else {
+				col._dataType.showFieldError(field,true);
+				go = false
+			}
+		}
+	}
+	
+	if (go) {
+		go = this.fireEvent('formValidateEvent',{
+			form: elForm,
+			oldData: this._oldEditRowValues,
+			newData: newValues,
+			oRecord: oRecord
+		});
+	}
+	
+	if (go) {
+		for(var key in newValues) {
+			oRecord[key] = newValues[key];
+		}
+		this.updateRow(oRecord,parseInt(oRecord.yuiRecordId.substr(9)));
+		go = this.fireEvent('formSubmitEvent',{
+			form: elForm,
+			oldData: this._oldEditRowValues,
+			newData: newValues,oRecord:oRecord
+		});
+	}
+	if (go) {
+		return true;
+	}
 	YAHOO.util.Event.stopEvent(ev);
-	YAHOO.util.Connect.setForm(this.recordEditForm);
-	YAHOO.util.Connect.asyncRequest('POST', this.recordEditForm.action,
-	arguments);
 	return false;
 };
 
 /**
-
-This is how it is used. First you create the form (dt contains a DataTable
-instance):
-
-dt.createEditForm('formPanel',undefined,dt.postEditForm,{
-success:function() {
-alert('success')
-},
-failure:function() {
-alert('failure')
-}
-});
-
-In this case, I will put it in a panel, initially hidden, and since the
-whole DataTable was loaded via Connection Manager, I will use the same
-server URL. Finally, I use the provided postEditForm callback function and
-provide it with the arguments required by asyncRequest. This is done only
-once per page.
-
-When I want to edit a record, I call:
-
-dt.showEditForm(dt.getRecordSet()._records[0],{someExtraData:123});
-
-In this case, I am calling it to edit the first record and provide the form
-with someExtraData to put into the form.
-
-Since I used postEditForm, whenever the user clicks Ok the form will be
-submitted via ConnectionManager as a post and the reply will come by the
-success callback function provided, in this case a couple of alerts.
-
-In the above functions I provided both for the editor:textbox and
-editor:textarea. For those wondering what YAHOO.widget.DataTypes is, see
-my 'feature request' at:
-
-https://sourceforge.net/tracker/?func=detail&atid=836479&aid=1684188&group_id=165715
- 
+* This function is provided as an option to submit the form via the XHR object.  
+* It is mean to be used as the function to call on the formSubmitEvent.
+* When doing so, it is necesary to povide the callback ofbject for the XHR reply:
+* <code>dt.dataTable.subscribe('formSubmitEvent', dt.dataTable.postEditForm, asyncRequestCallback);</code>
+ * 
+ * @method postEditForm
+ * @param ev {Event object}  (ignored)
+ * @param callback {object} as per YAHOO.util.Connect.asyncRequest callback object
  */
+
+			
+YAHOO.widget.DataTable.prototype.postEditForm = function(ev,callback) {
+	YAHOO.util.Connect.setForm(this._rowEditForm);   
+	YAHOO.util.Connect.asyncRequest('POST', this._rowEditForm.action, callback);
+	return false;
+};
