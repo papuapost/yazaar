e21217e21d3c708c294cc6ca1efaac0ad22fb3f7
ted.husted <ted.husted@0be7c2d7-182d-0410-87d6-19f808efe882>
2007-05-20 23:41:55 +0000

    Issue 18 - PIM Example: Add initial files.

---
+++ trunk/examples/pim/my.js
@@ -0,0 +1,67 @@
+/*
+Copyright (c) 2007, Husted dot Com, Inc. All rights reserved.
+Code licensed under the BSD License: http://developer.yahoo.net/yui/license.txt
+*/
+if (typeof parent.MY != "undefined") {
+    var MY = parent.MY; // Application Class namespace
+    var my = parent.my; // Application singleton
+} else {
+    var $ = YAHOO.util.Dom.get;
+
+    var MY = {};
+
+    var my = {};
+
+    MY.Events = function() {
+        this.createEvent("facilityList");
+        this.createEvent("staffList");
+        return this;
+    };
+    YAHOO.augment(MY.Events, YAHOO.util.EventProvider);
+
+    MY.View = function(sName,sTitle) {
+        var oView = new YAHOO.widget.Overlay(sName);
+        oView.oContent = null;
+        my.oViewManager.register(oView);
+        my.oViewManager.oViews[sTitle] = oView;
+        oView.render();
+        return oView;
+    };
+
+    my.info = function(sMessage) {
+        YAHOO.log(sMessage,"info");
+    };
+
+    my.error = function(sMessage) {
+        YAHOO.log(sMessage,"error");
+    };
+
+    my.warn = function(sMessage) {
+        YAHOO.log(sMessage,"warn");
+    };
+
+    my.oEvents = new MY.Events();
+
+    my.oEvents.onFacilityListReturn = function(oData) {
+        YAHOO.log("facilityList Event");
+        my.oEvents.fireEvent("facilityList", oData);
+    };
+
+    my.oEvents.onStaffListReturn = function(oData) {
+        YAHOO.log("staffList Event");
+        my.oEvents.fireEvent("staffList", oData);
+    };
+
+    my.oViewConfig = {
+        width:"800px",
+        fixedcenter: false,
+        constraintoviewport: true,
+        underlay:"shadow",
+        close:true,
+        visible:true,
+        draggable:true
+    };
+
+    my.oViewManager = new YAHOO.widget.OverlayManager();
+    my.oViewManager.oViews = {};
+}
