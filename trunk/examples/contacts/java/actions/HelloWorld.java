9003a3ecd74865f5b9bc0344732dae7f29602b34
ted.husted <ted.husted@0be7c2d7-182d-0410-87d6-19f808efe882>
2007-10-10 17:28:57 +0000

    Issue 27 - Second pass at contacts checkin.

--- trunk/examples/contacts/java/actions/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package actions;
-import com.opensymphony.xwork2.ActionSupport;
-
-public class HelloWorld extends ActionSupport {
-	public String getGreeting() {
-		return getText("greeting");
-	}
-}
