a03f7ba282bc5012ce7f931c703e695be094876b
ted.husted <ted.husted@0be7c2d7-182d-0410-87d6-19f808efe882>
2008-08-17 15:03:42 +0000

    Issue 28 - Tweak example code.

--- trunk/examples/struts-pim/java/action/ContactLoadAction.java
+++ trunk/examples/struts-pim/java/action/ContactLoadAction.java
@@ -3,34 +3,18 @@ package action;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
-
 import org.apache.struts2.config.ParentPackage;
 import org.apache.struts2.config.Result;
-
 import com.googlecode.jsonplugin.JSONResult;
 import com.googlecode.jsonplugin.annotations.JSON;
 
+@SuppressWarnings("unchecked")
 @ParentPackage("my-default")
 @Result(name="success", value="", type=JSONResult.class)
-@SuppressWarnings("unchecked")
 public class ContactLoadAction {
-
-/*
-MY.Contacts.prototype.LOCAL_DATA = {
-    result : [
-        {id: "c5b6bbb1-66d6-49cb-9db6-743af6627828", last_name: "Beeblebrox ", first_name: "Zaphod ", extension: "555-123-4565", username: "zaphie ", hired: "04/01/1978", hours: -1, editor: "1"},
-        {id: "7c424227-8e19-4fb5-b089-423cfca723e1", last_name: "Yooden", first_name: "Vranx", extension: "555-123-4566", username: "conker", hired: "04/01/1978", hours: 37.5, editor: "1"},
-        {id: "9320ea40-0c01-43e8-9cec-8fb9b3928c2c", last_name: "Halfrunt", first_name: "Gag", extension: "555-123-4567", username: "ziggy", hired: "04/01/1978", hours: 7, editor: "0"},
-        {id: "3b27c933-c1dc-4d85-9744-c7d9debae196", last_name: "Android", first_name: "Marvin", extension: "555-123-4568", username: "blue", hired: "06/15/1978", hours: 161, editor: "1"},
-        {id: "554ff9e7-a6f5-478a-b76b-a666f5c54e40", last_name: "McMillan", first_name: "Tricia", extension: "555-123-4569", username: "trillian", hired: "05/28/1978", hours: 37.5, editor:"0"}
-    ]
-};
- */
-
 	private List contactLoad = new ArrayList();
     @JSON(name="result")
 	public List getContactLoad() {
-
     	contactLoad.add(new Contact(
     			"c5b6bbb1-66d6-49cb-9db6-743af6627828",
     			"Beeblebrox",
@@ -41,7 +25,6 @@ MY.Contacts.prototype.LOCAL_DATA = {
     			"-1",
     			"1"
     	));
-
 		return contactLoad;
 	}
 
@@ -53,6 +36,7 @@ MY.Contacts.prototype.LOCAL_DATA = {
 
 		public Contact() {};
 
+		@SuppressWarnings("deprecation")
 		public Contact(String id, String last_name, String first_name,
 				String extension, String username, String hired, String hours, String editor)
 		{
--- trunk/examples/struts-pim/java/action/Greeting.java
+++ trunk/examples/struts-pim/java/action/Greeting.java
@@ -1,4 +1,4 @@
-package actions;
+package action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class Greeting extends ActionSupport {
